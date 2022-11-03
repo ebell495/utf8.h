b52304c44baa27b0b7a5907ba7ac48d6a9301ea1
ebell495 <ericbell495@gmail.com>
2022-11-02 20:42:16 -0700

    Integrate Mayhem

---
+++ fuzz/utf8h-fuzz.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../utf8.h"
+
+uint8_t tmpBuffer[8196];
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+    if (Size > 0) {
+        uint8_t* in_data = (uint8_t*) malloc(Size - 1);
+        memcpy(in_data, Data + 1, Size - 1);
+        if (Data[0] % 5 == 0) {
+            utf8cat(tmpBuffer, in_data);
+        } else if (Data[0] % 5 == 0) {
+            utf8cpy(tmpBuffer, in_data);
+        } else if (Data[0] % 5 == 0) {
+            utf8len(in_data);
+        } else if (Data[0] % 5 == 0) {
+            utf8size(in_data);
+        } else {
+            utf8valid(in_data);
+        }
+        free(in_data);
+    }
+    return 0;
+}
