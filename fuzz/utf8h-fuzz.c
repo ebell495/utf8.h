86b2bee33b251e43e5802346da2569ad856d71c4
ebell495 <ericbell495@gmail.com>
2022-11-02 21:15:35 -0700

    fix

--- fuzz/utf8h-fuzz.c
+++ fuzz/utf8h-fuzz.c
@@ -14,11 +14,11 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
         memcpy(in_data, Data + 1, Size - 1);
         if (Data[0] % 5 == 0) {
             utf8cat(tmpBuffer, in_data);
-        } else if (Data[0] % 5 == 0) {
+        } else if (Data[0] % 5 == 1) {
             utf8cpy(tmpBuffer, in_data);
-        } else if (Data[0] % 5 == 0) {
+        } else if (Data[0] % 5 == 2) {
             utf8len(in_data);
-        } else if (Data[0] % 5 == 0) {
+        } else if (Data[0] % 5 == 3) {
             utf8size(in_data);
         } else {
             utf8valid(in_data);
